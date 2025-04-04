d4a43ba2357efad4cd82036e01cadcfe84637bc4
Numka <numinor93@mail.ru>
2025-04-04 20:05:16 +0300

    Navigation fix

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,13 +1,16 @@
+import 'package:currency_convert/app/app_router.dart';
 import 'package:currency_convert/features/bottom_tab/view/bottom_tab_screen.dart';
 import 'package:currency_convert/l10n/arb/app_localizations.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
-  const App({super.key});
+  App({super.key});
+
+  final _appRouter = AppRouter();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       theme: ThemeData(
         appBarTheme: AppBarTheme(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -16,7 +19,7 @@ class App extends StatelessWidget {
       ),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      home: const BottomTabScreen(),
+      routerConfig: _appRouter.config(),
     );
   }
 }
--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -2,5 +2,5 @@ import 'package:currency_convert/app/app.dart';
 import 'package:currency_convert/bootstrap.dart';
 
 void main() {
-  bootstrap(() => const App());
+  bootstrap(() => App());
 }
