fac426f59ed5e5ca64e39aa62eb3d52c7fd83550
Numka <numinor93@mail.ru>
2025-04-04 21:06:11 +0300

    Small fixes to vscode reported problems

--- lib/features/conversion/data/remote/currency_repository.dart
+++ lib/features/conversion/data/remote/currency_repository.dart
@@ -8,8 +8,12 @@ class CurrencyRepository implements CurrencyInterface {
 
   @override
   Future<List<Currency>> getCurrencies() async {
-    final response = await dio.get('https://api.coincap.io/v2/rates');
-    final data = response.data['data'] as List;
-    return data.map((e) => Currency.fromMap(e as Map<String, dynamic>)).toList();
+    try {
+      final response = await dio.get<Map<String, dynamic>>('https://api.coincap.io/v2/rates');
+      final data = response.data!['data'] as List;
+      return data.map((e) => Currency.fromMap(e as Map<String, dynamic>)).toList();
+    } catch (e) {
+      throw e;
+    }
   }
 }
