aa716e84587cb22268352a052c4d9dc50c263e5a
Numka <numinor93@mail.ru>
2025-04-04 20:55:55 +0300

    hydrated bloc fix and added bloc providers

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,7 +1,10 @@
 import 'package:currency_convert/app/app_router.dart';
-import 'package:currency_convert/features/bottom_tab/view/bottom_tab_screen.dart';
+import 'package:currency_convert/features/conversion/data/remote/currency_repository.dart';
+import 'package:currency_convert/features/features.dart';
 import 'package:currency_convert/l10n/arb/app_localizations.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class App extends StatelessWidget {
   App({super.key});
@@ -10,16 +13,22 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => AuthCubit()..checkAuthStatus()),
+        BlocProvider(create: (_) => CurrencyListCubit(currencyRepo: CurrencyRepository(dio: Dio()))),
+      ],
+      child: MaterialApp.router(
+        theme: ThemeData(
+          appBarTheme: AppBarTheme(
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          ),
+          useMaterial3: true,
         ),
-        useMaterial3: true,
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+        routerConfig: _appRouter.config(),
       ),
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-      routerConfig: _appRouter.config(),
     );
   }
 }
--- lib/features/conversion/view/currency_list_screen.dart
+++ lib/features/conversion/view/currency_list_screen.dart
@@ -1,5 +1,8 @@
-import 'package:auto_route/annotations.dart';
-import 'package:flutter/widgets.dart';
+import 'package:auto_route/auto_route.dart';
+import 'package:currency_convert/app/app_router.dart';
+import 'package:currency_convert/features/features.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 @RoutePage()
 class CurrencyListScreen extends StatelessWidget {
@@ -7,6 +10,53 @@ class CurrencyListScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Currency Rates'),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.logout,
+              color: Theme.of(context).iconTheme.color,
+            ),
+            onPressed: () async {
+              context.read<AuthCubit>().logout();
+              await context.router.replace(const AuthRoute());
+            },
+          ),
+        ],
+      ),
+      body: BlocBuilder<CurrencyListCubit, CurrencyListState>(
+        builder: (context, state) {
+          if (state.status.isInitial || state.status.isLoading) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (state.status.isFailure) {
+            return Center(child: Text(state.errorMessage!));
+          } else if (state.status.isSuccess) {
+            return ListView.builder(
+              itemCount: state.currencies.length,
+              itemBuilder: (context, index) {
+                final currency = state.currencies[index];
+                return ListTile(
+                  title: Text(currency.symbol),
+                  subtitle: Text(currency.id),
+                  trailing: Text(
+                    currency.rateUsd.toStringAsFixed(2),
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                );
+              },
+            );
+          }
+          return const SizedBox.shrink();
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          context.read<CurrencyListCubit>().fetchCurrencies();
+        },
+        child: const Icon(Icons.refresh),
+      ),
+    );
   }
 }
